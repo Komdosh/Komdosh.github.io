001ed37714af43fe9d5d597edf38ac333670c1cd
Andrey Tabakov <komdosh@yandex.ru>
2018-03-17 15:13:38 +0300

    include existant code of mine website into angular app

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,13 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {FooterComponent} from './components/footer/footer.component';
+import {HeaderComponent} from './components/header/header.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
